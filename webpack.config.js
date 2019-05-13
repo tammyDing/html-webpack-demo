5fcff48dc211c603f56dc96d4d1078797076d627
dingtingting <dingtingting@gomefinance.com.cn>
2019-05-13 15:35:06 +0800

    多页面应用配置

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ module.exports = {
     mode: 'development',
     entry: {
         main: './src/js/main.js',
-        page: './src/js/page.js'
+        a: './src/js/a.js',
+        b: './src/js/b.js',
+        c: './src/js/c.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -18,13 +20,26 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: 'a.html', // 指定生成的文件名称：index-[hash].html
             template: 'index.html', // 指定根目录下的index文件
-            inject: 'head', // 指定js放在html的哪个位置
-            title: 'webpack title', // 定义title变量html页面通过<%= htmlWebpackPlugin.options.title %>获取
-            time: new Date(), // 定义time变量，获取方式同上
-            minify: {
-                removeComments: true, // 删除注释
-                collapseWhitespace: true // 删除空格
-            }
+            inject: false, // 指定js放在html的哪个位置
+            title: 'webpack a title', // 定义title变量html页面通过<%= htmlWebpackPlugin.options.title %>获取
+            // chunks: ['main', 'a'], // 指定当前页面引用的js
+            excludeChunks: ['b', 'c'], // 排除的js
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'b.html', // 指定生成的文件名称：index-[hash].html
+            template: 'index.html', // 指定根目录下的index文件
+            inject: false,
+            title: 'webpack b title',
+            // chunks: ['b']
+            excludeChunks: ['a', 'c'],
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'c.html', // 指定生成的文件名称：index-[hash].html
+            template: 'index.html', // 指定根目录下的index文件
+            inject: false, // 指定js放在html的哪个位置
+            title: 'webpack c title', // 定义title变量html页面通过<%= htmlWebpackPlugin.options.title %>获取
+            // chunks: ['c']
+            excludeChunks: ['a', 'b'],
         })
     ]
 }
